1a777a6dcd4b9ad9d2ff1ca6a8667b0b5679860b
Redither <slavonchik68@gmail.com>
2023-08-25 18:38:48 +0300

    fixes

--- js/gallery-grid.js
+++ js/gallery-grid.js
@@ -1,15 +1,14 @@
 var elem = document.querySelector('.gallery_grid-masonry');
-window.onload = function() {
-  if (window.innerWidth < 726) {
-      return
-  } else {
-    var msnry = new Masonry( elem, {
-      itemSelector: '.gallery_grid-item',
-      percentPosition: true,
-      columnWidth: ".gallery_grid-item",
-    //gutter: 20,
-    //fitWidth: true,
-    //   horizontalOrder: true
-    });
-  }
-}
+// window.onload = function() {
+//   if (window.innerWidth < 726) {
+//       return
+//   } else {}
+// }
+var msnry = new Masonry( elem, {
+  itemSelector: '.gallery_grid-item',
+  percentPosition: true,
+  columnWidth: ".gallery_grid-item",
+//gutter: 20,
+//fitWidth: true,
+//   horizontalOrder: true
+});
